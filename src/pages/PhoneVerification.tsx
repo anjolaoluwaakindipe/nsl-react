97a6204b0fcbbe0e15079729bb2f3dc3ff8a013d
Anjolaoluwa Daniel Akindipe <anjyakindipe@gmail.com>
2022-05-06 13:51:08 +0100

    update form validation on the PhoneVerification.tsx file in the page folder

--- src/pages/PhoneVerification.tsx
+++ src/pages/PhoneVerification.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import SingleTextField from '../components/shared/TextFields/SingleTextField';
 import DefaultLayout from '../components/layout/DefaultLayout';
@@ -6,45 +6,57 @@ import DefaultLayout from '../components/layout/DefaultLayout';
 // components
 import {Header} from '../components/pages/PhoneVerification'
 
+// form
+import { useForm, Controller } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
+
 
 
 function PhoneVerification() {
     // component state
-    const [verificationCodeText, setVerficationCodeText] = useState("");
+    const { control, handleSubmit} = useForm({resolver: joiResolver(Joi.object({phoneCode: Joi.string().required()}))})
 
-    // s
-    const verificationTextHandler: React.ChangeEventHandler<
-        HTMLInputElement
-    > = (e) => {
-        const text = e.target.value
-        if (text.length < 7) {
-            setVerficationCodeText(text);
-        }
+    const onSubmit = handleSubmit(data=>{
 
-        console.log(verificationCodeText);
-    };
+    })
 
     return (
         <DefaultLayout>
             <>
-                <Header/>
+                <Header />
+                <form onSubmit={onSubmit} autoComplete="off" autoSave='off'>
+                    <div className="pt-20 w-full md:w-1/2 space-y-6">
+                        <Controller
+                        name="phoneCode"
+                            control={control}
+                            render={({field:{onChange, value}}) => (
+                                <SingleTextField
+                                    placeholder="Verification Code"
+                                    onChange={onChange}
+                                    value={value}
+                                />
+                            )}
+                        />
 
-                <div className="pt-20 w-full md:w-1/2 space-y-6">
-                    <SingleTextField placeholder="Verification Code" onChange={verificationTextHandler} value={verificationCodeText}/>
+                        <h6>
+                            Didn't get code?{" "}
+                            <Link className="text-accentColor" to={"/"}>
+                                Resend
+                            </Link>
+                        </h6>
+                    </div>
 
-                    <h6>
-                        Didn't get code?{" "}
-                        <Link className="text-accentColor" to={"/"}>
-                            Resend
-                        </Link>
-                    </h6>
-                </div>
-
-                <div className="w-full pt-20">
-                    <button className=" w-full md:w-1/2 btn1" onClick={() => {}}>
-                        Verify
-                    </button>
-                </div>
+                    <div className="w-full pt-20">
+                        <button
+                            className=" w-full md:w-1/2 btn1"
+                            onClick={() => {}}
+                            type="submit"
+                        >
+                            Verify
+                        </button>
+                    </div>
+                </form>
             </>
         </DefaultLayout>
     );
