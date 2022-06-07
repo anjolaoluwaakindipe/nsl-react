139edbca114d8f4b7923210a539736a482d310aa
Damisi Babalola <dami600bab@gmail.com>
2022-06-07 10:28:04 +0100

    okjf

--- src/components/pages/UserDashboard/EmptyStatus.tsx
+++ src/components/pages/UserDashboard/EmptyStatus.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function EmptyStatus() {
     return (
-        <div className="md:p-10 p-5 w-full gap-20 flex justify-center bg-gradient-to-b from-white to-yellow-200  via-yellow-100 italic">
-            <h3 className=""> EMPTY</h3>
+        <div className="md:p-5 p-5 w-full gap-10 flex justify-center italic">
+            <h3>
+            <img
+                    src="assets/emptynote.svg"
+                    alt="no loans available "
+                    className="object-contain w-50 md:w-50 md:px-10  pb-5  "
+                   
+                />
+                 you don't have any pending requests</h3>
         </div>
     );
 }
