871b1520a4a07d5caa3be970626192c999941f8c
Damisi Babalola <dami600bab@gmail.com>
2022-06-09 10:08:17 +0100

    logout

--- src/components/layout/NavBarLayout.tsx
+++ src/components/layout/NavBarLayout.tsx
@@ -4,9 +4,16 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { VscBellDot } from "react-icons/vsc";
 import { paths } from "../../utils/constants/allPaths";
+import { useModal } from "../../services/customHooks/useModal";
 
 function NavBarLayout({ children }: { children?: React.ReactElement }) {
     const navigate = useNavigate();
+
+    const { openModalFunc } = useModal(
+        "LogOutModal",
+        false,
+    );
+
     return (
         <div>
             <div className="w-full py-5 shadow-2xl z-10 bg-bgColor">
@@ -19,10 +26,24 @@ function NavBarLayout({ children }: { children?: React.ReactElement }) {
                             onClick={() => navigate(paths.USER_DASHBOARD)}
                         />
                     </div>
+
                     <div className="flex space-x-5  md:space-x-10 justify-end items-center">
+                        {/* notification */}
                         <div className="w-6 h-6 md:w-10 md:h-10 flex justify-center items-center bg-red-100 hover:brightness-75   rounded-full cursor-pointer">
                             <VscBellDot className="md:text-2xl text-primaryColor" />
                         </div>
+
+                        {/*logout*/}
+                        <div>
+                            <button className="flex justify-center items-center  w-20 h-7 md:w-25 md:h-10 text-sm bg-primaryColor rounded-lg text-contColor cursor-pointer hover:brightness-75"
+                                onClick={() => {
+                                    openModalFunc();
+                                }}
+                            >
+                                LOG OUT
+                            </button>
+                        </div>
+                        {/*profile*/}
                         {/* <img src="" alt="" /> */}
                         <div className="flex justify-center items-center bg-primaryColor w-6 h-6 md:w-10 md:h-10  rounded-full cursor-pointer hover:brightness-75">
                             <CgProfile
