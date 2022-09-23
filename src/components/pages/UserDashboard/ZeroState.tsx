fb76112b4bad75db17a57740dd9607badd166c41
Anjolaoluwa Daniel Akindipe <anjyakindipe@gmail.com>
2022-09-23 15:43:33 +0100

    fix: added card check to loan zero state

--- src/components/pages/UserDashboard/ZeroState.tsx
+++ src/components/pages/UserDashboard/ZeroState.tsx
@@ -1,11 +1,54 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { paths } from "../../../utils/constants/allPaths";
+import { useSelector } from "react-redux";
+import { cardSelector } from "../../../state/redux/cardSlice";
+import { useModal } from "../../../services/customHooks/useModal";
+import toast from "react-hot-toast";
 
 function ZeroState() {
+    const cardInfoId = {
+        loadingState: "cardInfo.loading",
+        errorState: "cardInfo.error",
+    };
+    const { openModalFunc } = useModal("AddCardModal", true);
+    const cardstate = useSelector(cardSelector);
     const navigate = useNavigate();
     const navigateToLoanApplication = () => {
-        navigate(paths.LOAN_APPLICATION);
+        if (cardstate.isLoading) {
+            toast.loading("Getting your card info, please wait...", {
+                id: cardInfoId.loadingState,
+                position: "top-right",
+            });
+        }
+
+        if (
+            cardstate.cardList &&
+            cardstate.cardList.length === 0 &&
+            !cardstate.isLoading
+        ) {
+            openModalFunc();
+        }
+
+        if (
+            !cardstate.isError &&
+            cardstate.cardList &&
+            cardstate.cardList.length > 0 &&
+            !cardstate.isLoading
+        ) {
+            navigate(paths.LOAN_APPLICATION);
+            return;
+        }
+
+        if (cardstate.isError && !cardstate.isLoading) {
+            toast.error(
+                "An error occured while getting your card info. Please refresh the page",
+                {
+                    id: cardInfoId.errorState,
+                    position: "top-right",
+                }
+            );
+        }
     };
     return (
         <div className="bg-white relative flex flex-col-reverse md:flex-row items-center pt-10">
